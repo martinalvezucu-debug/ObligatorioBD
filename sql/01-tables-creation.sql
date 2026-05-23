713782c1ce8a818a4b78348ffb8dbb3541598da2
diegocastro1o <diegoandres.castro@correo.ucu.edu.uy>
2026-05-23 17:29:57 -0300

    Agrego definicion inicial de las tablas

---
+++ sql/01-tables-creation.sql
@@ -0,0 +1,79 @@
+DROP DATABASE IF EXISTS obligatorio;
+
+CREATE DATABASE obligatorio DEFAULT CHARACTER SET utf8 COLLATE utf8_spanish_ci;
+
+USE obligatorio;
+
+CREATE TABLE estudiantes (
+    id VARCHAR(15) PRIMARY KEY, # `id` es el documento del estudiante
+    nombre VARCHAR(50) NOT NULL,
+    apellido VARCHAR(50) NOT NULL,
+    email VARCHAR(75) NOT NULL UNIQUE,
+    carrera VARCHAR(100) NOT NULL,
+    facultad VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE disciplinas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE espacios (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(20) NOT NULL UNIQUE
+);
+
+CREATE TABLE actividades (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(50) NOT NULL UNIQUE,
+    id_disciplina INT NOT NULL,
+    id_espacio INT NOT NULL,
+    cupo_maximo INT NOT NULL,
+    dia VARCHAR(50) NOT NULL,
+    horario TIME NOT NULL,
+    estado VARCHAR(15) NOT NULL, # ejm: 'abierta', 'cerrada', 'finalizada' o 'cancelada'.
+
+    CONSTRAINT fk_disciplina_actividad
+    FOREIGN KEY (id_disciplina)
+    REFERENCES disciplinas(id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_espacio_actividad
+    FOREIGN KEY (id_espacio)
+    REFERENCES espacios(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE inscripciones (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_estudiante VARCHAR(15) NOT NULL,
+    id_actividad INT NOT NULL,
+    fecha_inscripcion DATE NOT NULL,
+    estado VARCHAR(25) NOT NULL, # ejm: 'espera', 'confirmada'
+
+    UNIQUE (id_estudiante, id_actividad),
+
+    CONSTRAINT fk_estudiante_inscripcion
+    FOREIGN KEY (id_estudiante)
+    REFERENCES estudiantes(id)
+    ON DELETE CASCADE,
+
+    CONSTRAINT fk_actividad_inscripcion
+    FOREIGN KEY (id_actividad)
+    REFERENCES actividades(id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE asistencias (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_inscripcion INT NOT NULL,
+    fecha DATE NOT NULL,
+    asistencia BOOL NOT NULL,
+
+    UNIQUE (id_inscripcion, fecha),
+
+    CONSTRAINT fk_inscripcion_asistencia
+    FOREIGN KEY (id_inscripcion)
+    REFERENCES inscripciones(id)
+    ON DELETE CASCADE
+);
